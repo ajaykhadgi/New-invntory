002e0f970a0bb66c84017aa2942181cdd3e8ed48
ajaykhadgi <102649828+ajaykhadgi@users.noreply.github.com>
2023-01-13 20:03:36 +0545

    Added mongoose schema and routes

--- Backend/Server.js
+++ Backend/Server.js
@@ -3,10 +3,24 @@ const cors = require("cors");
 const dotenv = require("dotenv").config();
 const bodyParser = require('body-parser');
 const mongoose = require("mongoose");
+const userRoute = require("./routes/userRoute");
+const { Router } = require("express");
 
 const app = express()
+//middleware
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+app.use(bodyParser.json())
+// router middler ware
+app.use("/api/users",userRoute);
+//Routs
+app.get("/",(req, res) => {
+    res.send("home Ajay page");
+
+})
 const PORT = process.env.PORT || 4000;
  // Connect to mongo db  and start servr //
+ mongoose.set('strictQuery', false);
  mongoose
  .connect(process.env.MONGO_URI)
  .then(() =>{
@@ -16,3 +30,4 @@ const PORT = process.env.PORT || 4000;
  })
  .catch((err)=> console.log(err))
 
+ module.exports=router;
